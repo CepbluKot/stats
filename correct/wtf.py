64c323e23262022fc60ea49432eb19b382bdfd24
Igor <igorcat100@gmail.com>
2024-04-17 21:28:29 +0300

    fixed

--- correct/wtf.py
+++ correct/wtf.py
@@ -17,19 +17,26 @@ def divide_to_same_intervals(data: list, n_parts: int):
             while end_id < len(data) and data[end_id] - data[begin_id] <= curr_interval:
                 end_id += 1
 
+            # print(curr_n_parts,data[begin_id-1], data[begin_id], '|',data[end_id-1],data[end_id], data[end_id+1])
             # all_intervals.append(data[begin_id:end_id])
+            if end_id-1 < len(data) and data[end_id-1] - data[begin_id] != curr_interval:
+                interval_is_ok = False
+                break
+
+            
             prev_begin_id = begin_id
             begin_id = end_id
-            end_id += 1 
             curr_n_parts += 1
 
-        if end_id >= len(data) and curr_n_parts <= n_parts:
+        if not interval_is_ok:
+            break
+
+        elif end_id >= len(data) and curr_n_parts <= n_parts:
             if data[len(data) - 1] - data[prev_begin_id] == curr_interval:
                 curr_interval += 1
         
             interval_is_ok = False
         
-
 
         else:
             curr_interval += 1
@@ -65,7 +72,7 @@ with open('/home/oleg/Documents/stats_lecs/correct/Москва_2021.txt') as fi
     data = [int(line.rstrip()) for line in file]
 
 
-res = divide_to_same_intervals(data, 7)
+res = divide_to_same_intervals(data, 10)
 
 for x in res:
     print (min(x)- max(x))
